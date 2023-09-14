71337b4ba785647418e208fffe134ae8502bd390
RoyTennenbaum <roytenen10@gmail.com>
2023-09-14 03:21:16 +0300

    adjust flex

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Main from '@/components/Main/Main';
 
 export default function Home() {
   return (
-    <main className="h-full">
+    <main className="flex h-full flex-col px-4 md:px-20 lg:px-40 xl:px-60">
       <Search />
       <Main />
     </main>
--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,16 +1,14 @@
 const Main: React.FC = () => {
   return (
-    <section className="flex h-fit items-center justify-center">
-      <section className="h-4/5 w-4/5">
-        <section className="flex items-center justify-between">
-          <div className="flex flex-col">
-            <span>Tel Aviv</span>
-            <span>38C</span>
-          </div>
-          <button>ADD TO FAVORITES</button>
-        </section>
-        <section></section>
+    <section className="flex grow items-start bg-white opacity-70">
+      <section className="flex grow justify-between">
+        <div className="flex flex-col items-stretch">
+          <span>Tel Aviv</span>
+          <span>38C</span>
+        </div>
+        <button>ADD TO FAVORITES</button>
       </section>
+      <section></section>
     </section>
   );
 };
