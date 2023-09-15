2a480715c4c21ff835e69da048760cf4db28844d
RoyTennenbaum <roytenen10@gmail.com>
2023-09-15 20:06:09 +0300

    urgently transfer to other pc - not done

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import CardList from './CardList';
 
 const weatherData = [
@@ -8,7 +9,52 @@ const weatherData = [
   { day: 'Thursday', temperature: '37°C' },
 ];
 
+interface CurrentProps {
+  LocalObservationDateTime: string;
+}
+
 const Content: React.FC = () => {
+  useEffect(() => {
+    const currentWeather = async (city, abortSignal: AbortSignal) => {
+      try {
+        const response = await fetch(
+          // `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${process.env.NEXT_PUBLIC_WEATHER_API}&q=${query}`,
+          'http://localhost:3001/tel-aviv-current',
+          {
+            signal: abortSignal,
+          }
+        );
+        if (!response.ok) {
+          throw new Error(
+            `Response is not OK: ${response.status}, ${response.statusText}`
+          );
+        }
+
+        const rawData = await response.json();
+        if (!Array.isArray(rawData)) {
+          throw new Error(
+            `Response data is not of the expected type: ${rawData}`
+          );
+        }
+
+        const data: DataProp[] = rawData.map((city: any) => ({
+          Key: city.Key,
+          LocalizedName: city.LocalizedName,
+        }));
+
+        setCities(
+          data.filter((data) =>
+            data.LocalizedName.toLowerCase().includes(query)
+          )
+        );
+      } catch (err) {
+        console.error('Unexpected error:', err);
+      }
+    };
+
+    return () => {};
+  }, []);
+
   return (
     <section className="flex grow flex-col bg-white">
       <section className="flex justify-between">
